f108d77605a62a110c04ef7d9d74ab20f4e8a1cf
hrithik <hrithik.b@beehyv.com>
2024-05-16 14:22:11 -0700

    Changed swagger doc. Currenly only get is supported

--- src/app/api/v1/__init__.py
+++ src/app/api/v1/__init__.py
@@ -10,11 +10,11 @@ from .users import router as users_router
 # from .videos import router as videos_router
 
 router = APIRouter(prefix="/v1")
-router.include_router(login_router)
-router.include_router(logout_router)
-router.include_router(users_router)
+# router.include_router(login_router)
+# router.include_router(logout_router)
+# router.include_router(users_router)
 router.include_router(posts_router)
 # router.include_router(videos_router)
-router.include_router(tasks_router)
-router.include_router(tiers_router)
-router.include_router(rate_limits_router)
+# router.include_router(tasks_router)
+# router.include_router(tiers_router)
+# router.include_router(rate_limits_router)
--- src/app/api/v1/posts.py
+++ src/app/api/v1/posts.py
@@ -2,6 +2,7 @@ from typing import Annotated, Any
 
 from fastapi import APIRouter, Depends, Request
 from fastapi.responses import FileResponse,JSONResponse, StreamingResponse
+from pydantic import Field
 from fastcrud.paginated import PaginatedListResponse, compute_offset, paginated_response
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -16,7 +17,7 @@ from ...schemas.user import UserRead
 
 from enum import Enum
 
-router = APIRouter(tags=["posts"])
+router = APIRouter(tags=["Posts"])
 
 from fastapi import FastAPI, File, UploadFile, Form, HTTPException
 from fastapi.responses import JSONResponse
@@ -31,54 +32,57 @@ class VideoFormat(str, Enum):
     mp4 = ".mp4"
     fbx = ".fbx"
 
-@router.post("/{username}/upload", response_model=PostRead, status_code=201)
-async def upload_video(
-    request: Request,
-    current_user: Annotated[UserRead, Depends(get_current_user)],
-    db: Annotated[AsyncSession, Depends(async_get_db)],
-    username:str,
-    title: str = Form(...),
-    description: str = Form(...),
-    file: UploadFile = File(...),
-):
-    # Validate file extension
-    [fileName, fileFormat] = file.filename.split(".")
-    print(fileFormat)
-    if fileFormat not in  ["mp4","fbx"]:
-        raise HTTPException(status_code=400, detail="Invalid file format. Only .mp4 and .fbx are allowed.")
-    db_user = await crud_users.get(db=db, schema_to_select=UserRead, username=username, is_deleted=False)
+# @router.post("/{username}/upload", response_model=PostRead, status_code=201,
+#              description="Add a video to the database by uploading it and getting the id. The id can be utilized in the get call retrieve it back")
+# async def upload_video(
+#     request: Request,
+#     current_user: Annotated[UserRead, Depends(get_current_user)],
+#     db: Annotated[AsyncSession, Depends(async_get_db)],
+#     username:str,
+#     title: str = Form(...),
+#     description: str = Form(...),
+#     file: UploadFile = File(...),
+# ):
+#     # Validate file extension
+#     [fileName, fileFormat] = file.filename.split(".")
+#     print(fileFormat)
+#     if fileFormat not in  ["mp4","fbx"]:
+#         raise HTTPException(status_code=400, detail="Invalid file format. Only .mp4 and .fbx are allowed.")
+#     db_user = await crud_users.get(db=db, schema_to_select=UserRead, username=username, is_deleted=False)
 
-    # Validate User
-    if db_user is None:
-        raise NotFoundException("User not found")
+#     # Validate User
+#     if db_user is None:
+#         raise NotFoundException("User not found")
 
-    if current_user["id"] != db_user["id"]:
-        raise ForbiddenException()
+#     if current_user["id"] != db_user["id"]:
+#         raise ForbiddenException()
     
-    #Add Entry to database with file path
-    post_internal_dict = {}
-    post_internal_dict["title"] = title
-    post_internal_dict["text"] = description if description else ""
-    post_internal_dict["media_url"] = UPLOAD_DIR
-    post_internal_dict["created_by_user_id"] = db_user["id"]
-    post_internal = PostCreateInternal(**post_internal_dict)
-    created_post: PostRead = await crud_posts.create(db=db, object=post_internal)
-    # Save the file
+#     #Add Entry to database with file path
+#     post_internal_dict = {}
+#     post_internal_dict["title"] = title
+#     post_internal_dict["text"] = description if description else ""
+#     post_internal_dict["media_url"] = UPLOAD_DIR
+#     post_internal_dict["created_by_user_id"] = db_user["id"]
+#     post_internal = PostCreateInternal(**post_internal_dict)
+#     created_post: PostRead = await crud_posts.create(db=db, object=post_internal)
+#     # Save the file
 
-    file_location = os.path.join(UPLOAD_DIR, str(created_post.id)+"."+str(fileFormat))
-    with open(file_location, "wb") as f:
-        f.write(await file.read())
+    # file_location = os.path.join(UPLOAD_DIR, str(created_post.id)+"."+str(fileFormat))
+    # with open(file_location, "wb") as f:
+    #     f.write(await file.read())
 
-    return created_post
+    # return created_post
 
-@router.get("/{username}/video/{id}")
+@router.get("/{username}/video/{id}", 
+            description="Get the video a video by choosing the right format and entering the username. \n The Id is specific to the video that has been posted and returns a dummy video in all other cases")
 # @cache(key_prefix="{username}_post_cache", resource_id_name="id")
 async def get_video(
     request: Request, 
-    username: str, 
-    id: int,
+    # current_user: Annotated[UserRead, Depends(get_current_user)],
     db: Annotated[AsyncSession, Depends(async_get_db)],
     format: VideoFormat,
+    username: str , 
+    id: int,
     ):
     db_user = await crud_users.get(db=db, schema_to_select=UserRead, username=username, is_deleted=False)
     if db_user is None:
@@ -86,13 +90,15 @@ async def get_video(
     db_post: PostRead | None = await crud_posts.get(
         db=db, schema_to_select=PostRead, id=id, created_by_user_id=db_user["id"], is_deleted=False
     )
+    # if current_user["id"] != db_user["id"]:
+    #     raise ForbiddenException()
     if db_post is None:
-        raise NotFoundException("Video not found")
-    print(db_post)
-    file_location = os.path.join(db_post['media_url'],str(id)+format)
-    print(file_location)
+        file_location = "videos/10.mp4"
+    else:
+        file_location = os.path.join(db_post['media_url'],str(id)+format)
     if not os.path.isfile(file_location):
-        raise HTTPException(status_code=404, detail="Video not found")
+        # raise HTTPException(status_code=404, detail="Video not found")
+        file_location = "videos/10.mp4"
     # This alternative return gives us download
     # return FileResponse(path=file_location, media_type='application/octet-stream',filename=str(id)+format)
     media_type = "video/" + format[1:]
